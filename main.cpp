59d5080db5b5c0f5dedc7c4582c8482e47819278
Daniel <e.daniel.velazquez@gmail.com>
2017-04-06 19:19:36 -0500

    Actualice los autores

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include "Clases/Lista.h"
 
 using namespace std;
-/*proyecto de taller de sistemas operativos*/
+/*proyecto de taller de sistemas operativos
+ * Velazquez Machuca Eduardo Daniel (El hizo todo)
+ * Ramirez Rosales Everardo (El no hizo nada)
+ * Zuñiga Ramos Dulce Adelina (Ella chambeo, pa traer cocas)
+ * Romero Vazquez Jorge Alberto (Lo llevo en mi corazon)
+ * */
 int main() {
     Lista *l = new Lista();
     cout << "inserta elementos a la lista" << endl;
