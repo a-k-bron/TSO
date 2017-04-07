9adaeb3421fa6ba551781d1db10e03fa7559d695
Everardo Ramirez <everardoramirez@live.com.mx>
2017-04-06 19:14:39 -0500

    comentario

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Clases/Lista.h"
 
 using namespace std;
-
+/*proyecto de taller de sistemas operativos*/
 int main() {
     Lista *l = new Lista();
     cout << "inserta elementos a la lista" << endl;
