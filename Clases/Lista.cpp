5822611115f970b04adea600ce6612d260dd3bb6
Daniel <e.daniel.velazquez@gmail.com>
2017-05-09 18:13:16 -0500

    se jodio todo :(

--- Clases/Lista.cpp
+++ Clases/Lista.cpp
@@ -1,6 +1,3 @@
-//
-// Created by Everardo Ramirez on 30/03/2017.
-//
 
 #include "Lista.h"
 #include <iostream>
@@ -8,14 +5,14 @@
 using namespace std;
 
 Lista::Lista() {
-    primero = nullptr;
+    primero = nullptr; // marca el primero a nulo
 }
 
 
-Nodo *Lista::ultimo() {
+Nodo *Lista::ultimo() { // devuelve puntero al ultimo elemento de la lista
     Nodo *p = primero;
     if (p == nullptr)throw "Error, lista vacia";
-    while (p->getEnlace() != nullptr) {
+    while (p->getEnlace() != nullptr) { //apunta al siguiente nodo hasta apuntar al ultimo y llegar a nulo
         p = p->getEnlace();
     }
     return p;
@@ -45,9 +42,9 @@ void Lista::recorreLista() {
 
 void Lista::insertarInicio(string tipo, int posicion, int uam, int id, int tamano, int residuo) {
     Nodo *nuevo = new Nodo(uam, posicion, id, tamano, residuo, tipo);
-    if (primero == nullptr) {
+    if (primero == nullptr) { //inserta primer nodo al inicio de la lista si esta vacia
         primero = nuevo;
-    } else {
+    } else { // si no esta vacia da la primer posicion al ultimo en llegar, moviendo las posiciones de los demas
         nuevo->setEnlace(primero);
         primero = nuevo;
     }
@@ -56,17 +53,17 @@ void Lista::insertarInicio(string tipo, int posicion, int uam, int id, int taman
 
 void Lista::insertarMedio(string tipo, int posicion, int uam, int id, int tamano, int residuo, Nodo *anterior) {
     Nodo *nuevo = new Nodo(uam, posicion, id, tamano, residuo, tipo);
-    nuevo->setEnlace(anterior->getEnlace());
+    nuevo->setEnlace(anterior->getEnlace());//el metodo pide el enlace anterior, es decir la posicion en donde entrara el nuevo nodo y se posiciona delante de ese nodo.
     anterior->setEnlace(nuevo);
 
 }
 
-Nodo *Lista::getPrimero() const {
+Nodo *Lista::getPrimero() const { //obtiene el primer elemento de la lista
     return primero;
 }
 
 Nodo *Lista::buscarPorID(int buscado) {
-    Nodo *aux = getPrimero();
+    Nodo *aux = getPrimero();//busca hasta llegar al nodo buscado recorriendo toda la lista
     while (aux != nullptr) {
         if (aux->getId() == buscado) {
             return aux;
@@ -82,26 +79,26 @@ void Lista::eliminar(string tipo) {
     actual = getPrimero();
     anterior = nullptr;
     encontrado = false;
-    while ((actual != nullptr) && !encontrado) {
-        encontrado = (actual->getTipo() == tipo);
-        if (!encontrado) {
+    while ((actual != nullptr) && !encontrado) {// mientras no lleguemos al final de la lista y no encontremos el numero
+        encontrado = (actual->getTipo() == tipo);//comparo tipo del nodo actual con el tipo a buscar
+        if (!encontrado) {//mientras no lo encuentre sigo recorriendo la lista.
             anterior = actual;
             actual = actual->getEnlace();
         }
     }
 
-    if (actual != nullptr) {
+    if (actual != nullptr) {//siempre mantengo el primer puntero para evitar errores y perder la lista
         if (actual == primero) {
-            primero = actual->getEnlace();
+            primero = actual->getEnlace();//si es el primero, da la posicion del primero al siguiente
         } else {
-            anterior->setEnlace(actual->getEnlace());
+            anterior->setEnlace(actual->getEnlace());//le da la posicion al que esta despues al que quiero eliminar.
         }
         delete actual;
     }
 
 }
 
-void Lista::eliminar(int id) {
+void Lista::eliminar(int id) { //lo mismo que el anterior pero este elimina por el id
     Nodo *actual, *anterior;
     bool encontrado;
     actual = getPrimero();
--- Clases/SuperLista.cpp
+++ Clases/SuperLista.cpp
@@ -26,44 +26,36 @@ SuperLista::SuperLista(int tamanoMemoria, int tamanoUAM) : tamanoMemoria(tamanoM
     l = new Lista();
     l->insertarUltimo("h", 0, getTamanoMemoria() / getTamanoUAM(), -1, getTamanoMemoria(),
                       getTamanoMemoria() % getTamanoUAM());
-    contadorID = 0;
+    contadorID = 0;//Genera lista y mete el primero, el cual es el hueco de toda la memoria
 }
 
 void SuperLista::crearLista() {
 
     int tamano = 0;
     int calculoUAM, residuo;
-    Nodo *p;
     cout << "tamano del proceso" << endl;
     cin >> tamano;
     while (tamano != -1 && !lleno()) {
 
-        calculoUAM = tamano / getTamanoUAM();
+        calculoUAM = tamano / getTamanoUAM();//obtiene cuantas uams necesita el proceso
         if (tamano % getTamanoUAM() > 0) {
-            calculoUAM += 1;
-            residuo = getTamanoUAM() - (tamano % getTamanoUAM());
+            calculoUAM += 1;//si hay residuo, se le otorga otra uam
+            residuo = getTamanoUAM() - (tamano % getTamanoUAM());//muestra el residuo no usado de la uam
         } else {
-            residuo = 0;
+            residuo = 0;//en caso contrario no hubo residuo
         }
         if (getMayorHueco()->getUam() >= calculoUAM) {
-            Nodo *p;
-            int acumulado = 0;
             l->insertarInicio("p", 0, calculoUAM, contadorID, tamano, residuo);//tipo,posicion,uam,id,tamaño,residuo
-            p = l->getPrimero();
-            while (p != nullptr) {
-                p->setPosicion(acumulado);
-                acumulado += p->getUam();
-                p = p->getEnlace();
-            }
-            l->ultimo()->setUam(l->ultimo()->getUam() - calculoUAM);
-            l->ultimo()->setTamano(l->ultimo()->getTamano() - (calculoUAM * getTamanoUAM()));
+            actualizarPosiciones();
+            l->ultimo()->setUam(l->ultimo()->getUam() - calculoUAM);//obtiene el ultimo nodo que representa la memoria libre, actualiza las uam restando las aum que habia menos el tamaño del nuevo proceso
+            l->ultimo()->setTamano(l->ultimo()->getTamano() - (calculoUAM * getTamanoUAM()));//hace lo mismo que el anterior pero usando el tamaño de la memoria (bytes)
             contadorID++;
-        } else { cout << "falta espacio" << endl; }
+        } else { cout << "falta espacio" << endl; }//cuando no cabe
         cin >> tamano;
     }
 
-    if (l->ultimo()->getUam() == 0) {
-        l->ultimo()->setTipo("eliminar");
+    if (l->ultimo()->getUam() == 0) {//si el ultimo hueco tiene 0 uam, se convierte el nodo en el tipo eliminar.
+        l->ultimo()->setTipo("eliminar");//cambia el nombre del ultimo proceso a "eliminar" y elimina los tipos "eliminar"
         l->eliminar("eliminar");
     }
 }
@@ -76,27 +68,14 @@ void SuperLista::setContadorID(int contadorID) {
     SuperLista::contadorID = contadorID;
 }
 
-void SuperLista::buscar() {
-    int aux;
-    cout << "numero con que buscar: ";
-    cin >> aux;
-    l->buscarPorID(aux)->toString();
-
-}
-
 void SuperLista::terminarProceso(int id) {
-    Nodo *p = l->getPrimero();
+    Nodo *p = l->getPrimero(); //puntero que apunta al primer nodo
     int acumulado = 0;
-    if (l->buscarPorID(id)) {
+    if (l->buscarPorID(id)) {//busco que exista el proceso
         l->buscarPorID(id)->setTipo("h");//cambio el tipo a h
         l->buscarPorID(id)->setId(-1);
         terminarProceso();
-
-        while (p != nullptr) {
-            p->setPosicion(acumulado);
-            acumulado += p->getUam();
-            p = p->getEnlace();
-        }
+        actualizarPosiciones();
     } else {
         cout << "el proceso " << id << " no existe" << endl;
     }
@@ -109,18 +88,18 @@ void SuperLista::terminarProceso() {
     Nodo *p = l->getPrimero();//proceso de verificacion de huecos juntos
     while (p != nullptr && p->getEnlace() != nullptr) {
         if (p->getTipo() == "h" && p->getEnlace()->getTipo() == "h") {
-            p->setTamano(p->getTamano() + p->getEnlace()->getTamano());
-            calculoUAM = p->getTamano() / getTamanoUAM();
+            p->setTamano(p->getTamano() + p->getEnlace()->getTamano());//el tamaño del hueco es igual al nodo actual mas el nodo que sigue
+            calculoUAM = p->getTamano() / getTamanoUAM();//saca el tamaño en uams
             if (p->getTamano() % getTamanoUAM() > 0) {
                 calculoUAM += 1;
                 p->setResiduo(getTamanoUAM() - (p->getTamano() % getTamanoUAM()));
             } else {
                 p->setResiduo(0);
             }
-            p->setUam(calculoUAM);
+            p->setUam(calculoUAM); //deja el tamaño de uam a ese hueco
             p->getEnlace()->setTipo("eliminar");
             l->eliminar("eliminar");
-            terminarProceso();
+            terminarProceso();// se llama recursivamente para evitar que si hay tres huecos, solo se unan 2 y garantizar que la funcion se realice hasta que se cumpla el while
             //if (p == l->ultimo()) { break; }
         }
 
@@ -132,7 +111,7 @@ void SuperLista::aumentarContadorID() {
     contadorID++;
 }
 
-bool SuperLista::lleno() {
+bool SuperLista::lleno() {//obtiene las uams de los procesos y si la uam obtenida es igual a la memoria total, esta lleno
     int contador = 0;
     Nodo *p = l->getPrimero();
 
@@ -147,25 +126,25 @@ bool SuperLista::lleno() {
 
 }
 
-int SuperLista::getTamanoMemoriaUAM() {
+int SuperLista::getTamanoMemoriaUAM() {//regresa el tamaño de la memoria en UAM
     return getTamanoMemoria() / getTamanoUAM();
 }
 
 Nodo *SuperLista::getMayorHueco() {
-    Nodo *p = l->getPrimero(), *mayorTemporal;
-    while (p != nullptr) {
+    Nodo *p = l->getPrimero(), *mayorTemporal=nullptr;
+    while (p != nullptr) {//se usa el while hasta obtener el primer hueco
         if (p->getTipo() == "h") {
             mayorTemporal = p;
             break;
         }
-        p = p->getEnlace();
+        p = p->getEnlace();//en caso de no encontrar hueco pasa al siguiente enlace
     }
-    p = l->getPrimero();
+    p = l->getPrimero();//volvemos a poner p a primero
     while (p != nullptr) {
-        if (p->getUam() > mayorTemporal->getUam()) {
-            mayorTemporal = p;
+        if (p->getUam() > mayorTemporal->getUam() && p->getTipo() == "h") {
+            mayorTemporal = p; //se realiza la busqueda utilizando como referencia el primer hueco, si este es mayor ahora ese es el mayor temporal
         }
-        p = p->getEnlace();
+        p = p->getEnlace();//se busca ahora con el siguiente enlace hasta llegar al final de la lista y encontrar el mayor
     }
 
     return mayorTemporal;
